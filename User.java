e9f4bff9c9d180da66e9a0cada59a1dc01d56337
Bhartendu <aimlproject007@gmail.com>
2025-02-24 21:49:21 +0530

    Create User.java
    Added User entity

---
+++ User.java
@@ -0,0 +1,31 @@
+package com.travel.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Column(nullable = false, unique = true)
+    private String username;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    private String role = "USER";
+}
